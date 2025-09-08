7d3a3609af0859138c5c55937ab4106336a8d961
Gustavo Lopes <gustavorobertodealmeidalopes@gmail.com>
2025-09-08 16:27:31 -0300

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "dentibot";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco: " . $e->getMessage());
+}
